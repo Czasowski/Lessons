ec7d92741365f50a56c973528f2ced3476eb2f24
Wlad Czasowski <czasowskii@gmail.com>
2019-12-04 14:47:37 +0200

    add 5 chapter

--- src/Chapter5.java
+++ src/Chapter5.java
@@ -1,2 +1,43 @@
-public class Chapter5 {
+class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
+    }
+
+public String checkYourself(String stringGuess) {
+    int guess = Integer.parseInt(stringGuess);
+
+            String result = "miss";
+    for (int cell : locationCells) {
+        if (guess == cell) {
+            result = "hit";
+            numOfHits++;
+            break;
+        }
+    }
+    if (numOfHits == locationCells.length) {
+        result = "kill";
+    }
+    System.out.println(result);
+    return result;
 }
+}
+
+class TesterDotCom {
+        public static void main(String[] args) {
+SimpleDotCom dot = new SimpleDotCom();
+
+int[] locations = {2,3,4};
+dot.setLocationCells(locations);
+
+String userGuess = "5";
+String result = dot.checkYourself(userGuess);
+String testResult = "failed";
+if (result.equals("hit")) {
+    testResult = "passed";
+}
+            System.out.println(testResult);
+        }
+    }
